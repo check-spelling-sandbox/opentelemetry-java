7663966d6c8422f218380a3ae05c8acab7784429
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-01-22 09:09:03 -0500

    spelling: doesn't
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- sdk/trace/build.gradle.kts
+++ sdk/trace/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
 
   jmh(project(":sdk:metrics"))
   jmh(project(":sdk:testing")) {
-    // JMH doesn"t handle dependencies that are duplicated between the main and jmh
+    // JMH doesn't handle dependencies that are duplicated between the main and jmh
     // configurations properly, but luckily here it"s simple enough to just exclude transitive
     // dependencies.
     isTransitive = false
